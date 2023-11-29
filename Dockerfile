ce52befd2fc9d23c410f2a81ecac31eb4f23ea0c
cruizba <carlos.ruizbal@gmail.com>
2023-11-29 17:37:13 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM golang:1.21.4 AS builder
+
+ARG TARGETOS
+ARG TARGETPLATFORM
+ARG TARGETARCH
+RUN echo building execute for "$TARGETPLATFORM"
+
+WORKDIR /workspace
+
+# Cache dependencies
+RUN go env -w GOCACHE=/go-cache
+RUN go env -w GOMODCACHE=/gomod-cache
+
+COPY ./go.* ./
+RUN --mount=type=cache,target=/gomod-cache \
+  go mod download
+
+COPY . .
+
+RUN --mount=type=cache,target=/gomod-cache --mount=type=cache,target=/go-cache \
+    GOOS=$TARGETOS GOARCH=$TARGETARCH CGO_ENABLED=0 \
+    go build
+
+FROM alpine
+
+COPY --from=builder /workspace/can-you-handle-it-redis /app/
+
+ENTRYPOINT ["/app/can-you-handle-it-redis"]
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func main() {
+	// Define command-line flags
+	clusterNodes := flag.String("cluster", "", "Comma-separated list of Redis cluster node addresses")
+	numberOfGoroutines := flag.Int("goroutines", 10, "Number of goroutines to run")
+	password := flag.String("password", "", "Redis password")
+	sleep := flag.String("sleep", "1s", "Sleep duration between writes")
+	sleepDuration, err := time.ParseDuration(*sleep)
+	if err != nil {
+		fmt.Println("Error parsing sleep duration:", err)
+		os.Exit(1)
+	}
+
+	// Parse the flags
+	flag.Parse()
+
+	if *clusterNodes == "" {
+		fmt.Println("Please provide a comma-separated list of Redis cluster node addresses")
+		os.Exit(1)
+	}
+
+	if *password == "" {
+		fmt.Println("Please provide a Redis password")
+	}
+
+	// Split the cluster nodes string into a slice
+	nodes := strings.Split(*clusterNodes, ",")
+
+	// Create a Redis cluster client
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Password: *password,
+		Addrs:    nodes,
+	})
+
+	// Create a cancellable context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < *numberOfGoroutines; i++ {
+		wg.Add(1)
+		go func(ctx context.Context, rdb *redis.ClusterClient, wg *sync.WaitGroup) {
+			defer wg.Done()
+			for {
+				time.Sleep(sleepDuration)
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					err := rdb.Set(ctx, "key", "value", 0).Err()
+					if err != nil {
+						fmt.Println("Error writing to Redis:", err)
+						continue
+					}
+
+					val, err := rdb.Get(ctx, "key").Result()
+					if err != nil {
+						fmt.Println("Error reading from Redis:", err)
+						continue
+					}
+
+					fmt.Println("Got value:", val)
+				}
+			}
+		}(ctx, rdb, &wg)
+	}
+
+	// Set up signal handling
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-sigChan
+	fmt.Println("Shutting down...")
+	cancel()  // Cancel the context, signaling all goroutines to stop
+	wg.Wait() // Wait for all goroutines to finish
+}
